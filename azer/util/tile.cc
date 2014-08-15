75eb4cffa59d40b46ea5127cdc16762d9d250040
lenxyang <lenxyang@gmail.com>
2014-08-15 17:40:34 +0800

    .

---
+++ azer/util/tile.cc
@@ -0,0 +1,37 @@
+#include "azer/util/tile.h"
+
+namespace azer {
+namespace util {
+
+void Tile::Init() {
+  InitVertex();
+}
+
+void Tile::InitVertex() {
+  const int kRow = kCellNum_;
+  const int kColumn = kCellNum_;
+  const int kVertexNum = kRow * kColumn;
+  for (int i = 0; i < kColumn; ++i) {
+    for (int j = 0; j < kRow; ++j) {
+      int idx = i * kRow + j;
+      float x = (float)j - kRow / 2.0f;
+      float z = (float)i - kColumn / 2.0f;
+      vertices_.push_back(azer::vec3(x, 0.0f, z));
+    }
+  }
+
+  const int kTriangleNum = (kRow - 1) * (kColumn - 1) * 2;
+  for (int i = 0; i < kColumn-1; ++i) {
+    for (int j = 0; j < kRow-1; ++j) {
+      indices_.push_back(i * kRow + j);
+      indices_.push_back((i + 1) * kRow + j + 1);
+      indices_.push_back((i + 1) * kRow + j);
+      indices_.push_back(i * kRow + j);
+      indices_.push_back(i * kRow + j + 1);
+      indices_.push_back((i + 1) * kRow + j + 1);
+    }
+  }
+}
+
+}  // namespace azer {
+}  // namespace util
---
+++ azer/util/tile.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "base/files/file_path.h"
+#include "azer/render/render.h"
+
+namespace azer {
+namespace util {
+class Tile {
+ public:
+  Tile(const int cell = 1024) : kCellNum_(cell) {}
+
+  void Init();
+  const std::vector<azer::Vector3>& vertices() { return vertices_;}
+  const std::vector<int32>& indices() { return indices_;}
+
+  int GetCellNum() const { return kCellNum_;}
+ private:
+  void InitVertex();
+  std::vector<azer::Vector3> vertices_;
+  std::vector<int32> indices_;
+  const int kCellNum_;
+  DISALLOW_COPY_AND_ASSIGN(Tile);
+};
+
+}  // namespace util
+}  // namespace azer
