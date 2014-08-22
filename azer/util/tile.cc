84d9f286b51b1a9346539f89a327b4ca7cca25e2
lenxyang <lenxyang@gmail.com>
2014-08-22 10:28:18 +0800

    add quadtree

--- azer/util/tile.cc
+++ azer/util/tile.cc
@@ -1,7 +1,7 @@
 #include "azer/util/tile.h"
 
 #include <limits>
-#include "base/logging.h"
+#include "azer/render/render.h"
 
 namespace azer {
 namespace util {
@@ -9,26 +9,12 @@ namespace util {
 void Tile::Init() {
   min_x_ = std::numeric_limits<float>::max();
   max_x_ = -std::numeric_limits<float>::max();
+  min_y_ = std::numeric_limits<float>::max();
+  max_y_ = -std::numeric_limits<float>::max();
   min_z_ = std::numeric_limits<float>::max();
   max_z_ = -std::numeric_limits<float>::max();
+  yspec_ = false;
   InitVertex();
-}
-
-float Tile::minx() const {
-  DCHECK_GT(vertices_.size(), 0u);
-  return min_x_;
-}
-float Tile::maxx() const {
-  DCHECK_GT(vertices_.size(), 0u);
-  return max_x_;
-}
-float Tile::minz() const {
-  DCHECK_GT(vertices_.size(), 0u);
-  return min_z_;
-}
-float Tile::maxz() const {
-  DCHECK_GT(vertices_.size(), 0u);
-  return max_z_;
 }
 
 void Tile::InitVertex() {
@@ -89,6 +75,60 @@ void Tile::CalcNormal() {
 
 void Tile::SetHeight(int x, int z, float height) {
   vertices_[x * kCellNum_ + z].y = height;
+  if (height < min_y_) min_y_ = height;
+  if (height > max_y_) max_y_ = height;
+  yspec_ = true;
+}
+
+void QuadTree::SplitPitch(Node* node) {
+  DCHECK_GT(tail_, 0);
+  DCHECK_GE(node->level, 0);
+  Node* nodes = nodes_.get();
+  Node& n1 = nodes[tail_ + 0];
+  Node& n2 = nodes[tail_ + 1];
+  Node& n3 = nodes[tail_ + 2];
+  Node& n4 = nodes[tail_ + 3];
+  node->children[0] = tail_;
+  node->children[1] = tail_ + 1;
+  node->children[2] = tail_ + 2;
+  node->children[3] = tail_ + 3;
+  node->splitted = true;
+  tail_ += 4;
+
+  
+  int midx = node->midx();
+  int midy = node->midy();
+  n1.pitch.left = node->pitch.left;
+  n1.pitch.right = midx;
+  n1.pitch.top = node->pitch.top;
+  n1.pitch.bottom = midy;
+  n1.level = node->level - 1;
+  n2.splitted = false;
+
+  n2.pitch.left = midx;
+  n2.pitch.right = node->pitch.right;
+  n2.pitch.top = node->pitch.top;
+  n2.pitch.bottom = midy;
+  n2.level = node->level - 1;
+  n2.splitted = false;
+
+  n3.pitch.left = node->pitch.left;
+  n3.pitch.right = midx;
+  n3.pitch.top = midy;
+  n3.pitch.bottom = node->pitch.bottom;
+  n3.level = node->level - 1;
+  n3.splitted = false;
+
+  n4.pitch.left = midx;
+  n4.pitch.right = node->pitch.right;
+  n4.pitch.top = midy;
+  n4.pitch.bottom = node->pitch.bottom;
+  n4.level = node->level - 1;
+  n4.splitted = false;
+}
+
+bool FrustrumSplit::Split(const QuadTree::Node& node) {
+  return true;
 }
 }  // namespace azer {
 }  // namespace util
--- azer/util/tile.h
+++ azer/util/tile.h
@@ -2,9 +2,11 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 #include <cmath>
 
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "azer/render/render.h"
 
 namespace azer {
@@ -20,6 +22,7 @@ class Tile {
   const std::vector<azer::Vector3>& vertices() { return vertices_;}
   const std::vector<azer::Vector3>& normal() { return normal_;}
   const std::vector<int32>& indices() { return indices_;}
+
   void SetHeight(int x, int z, float height);
 
   int GetCellNum() const { return kCellNum_;}
@@ -29,9 +32,14 @@ class Tile {
 
   float minx() const;
   float maxx() const;
+  float x_range() const { return maxx() - minx();}
+
+  float miny() const;
+  float maxy() const;
+  float y_range() const { return maxy() - miny();}
+  
   float minz() const;
   float maxz() const;
-  float x_range() const { return maxx() - minx();}
   float z_range() const { return maxz() - minz();}
  private:
   void InitVertex();
@@ -40,8 +48,122 @@ class Tile {
   std::vector<int32> indices_;
   const int kCellNum_;
   const int kLevel_;
-  float min_x_, max_x_, min_z_, max_z_;
+  float min_x_, max_x_, min_y_, max_y_, min_z_, max_z_;
+  bool yspec_;
   DISALLOW_COPY_AND_ASSIGN(Tile);
 };
+
+class QuadTree {
+ public:
+  struct Pitch {
+    int left;
+    int right;
+    int top;
+    int bottom;
+
+    Pitch(): left(-1), right(-1), top(-1), bottom(-1) {}
+    Pitch(const Pitch& pitch) {*this = pitch; }
+    Pitch& operator = (const Pitch& pitch);
+  };
+
+  struct Node {
+    Pitch pitch;
+    int children[4];
+    int level;
+    bool splitted;
+    Node(): level(-1), splitted(false) {}
+    int midx() const { return std::pow(2.0, level - 1) +  pitch.left;}
+    int midy() const { return std::pow(2.0, level - 1) +  pitch.top;}
+  };
+
+  explicit QuadTree(int level)
+      : tail_(-1)
+      , kLevel(level)
+      , kCellNum(std::pow(2.0, level) + 1) {
+    int max_cell = (int)std::pow(4.0, level) / 3;
+    nodes_.reset(new Node[max_cell]);
+  }
+
+  class Splitable {
+   public:
+    virtual bool Split(const Node& node) = 0;
+  };
+  void Split(Splitable* splitable, std::deque<Node*>* nodes);
+ private:
+  void InitNode();
+  void SplitPitch(Node* node);
+  int tail_;
+  std::unique_ptr<Node[]> nodes_;
+  const int kLevel;
+  const int kCellNum;
+  DISALLOW_COPY_AND_ASSIGN(QuadTree);
+};
+
+class FrustrumSplit : public QuadTree::Splitable {
+ public:
+  FrustrumSplit(Tile* tile) : tile_(tile) {}
+  virtual bool Split(const QuadTree::Node& node) OVERRIDE;
+ private:
+  Tile* tile_;
+  DISALLOW_COPY_AND_ASSIGN(FrustrumSplit);
+};
+
+inline void QuadTree::InitNode() {
+  tail_ = 0;
+  Node& n = (nodes_.get())[0];
+  n.splitted = false;
+  n.pitch.left = 0;
+  n.pitch.top = 0;
+  n.pitch.right = kCellNum;
+  n.pitch.bottom = kCellNum;
+}
+
+inline void QuadTree::Split(Splitable* splitable, std::deque<Node*>* final) {
+  InitNode();
+  int cur = tail_;
+  Node* node = nodes_.get();
+  while (cur <= tail_) {
+    if (!splitable->Split(*node)) {
+      final->push_back(node);
+    }
+    cur++;
+    node++;
+  }
+}
+
+inline QuadTree::Pitch& QuadTree::Pitch::operator = (const Pitch& pitch) {
+  left = pitch.left;
+  right = pitch.right;
+  top = pitch.top;
+  bottom = pitch.bottom;
+  return *this;
+}
+
+inline float Tile::minx() const {
+  DCHECK_GT(vertices_.size(), 0u);
+  return min_x_;
+}
+inline float Tile::maxx() const {
+  DCHECK_GT(vertices_.size(), 0u);
+  return max_x_;
+}
+inline float Tile::miny() const {
+  DCHECK(yspec_);
+  DCHECK_GT(vertices_.size(), 0u);
+  return min_y_;
+}
+inline float Tile::maxy() const {
+  DCHECK(yspec_);
+  DCHECK_GT(vertices_.size(), 0u);
+  return max_y_;
+}
+inline float Tile::minz() const {
+  DCHECK_GT(vertices_.size(), 0u);
+  return min_z_;
+}
+inline float Tile::maxz() const {
+  DCHECK_GT(vertices_.size(), 0u);
+  return max_z_;
+}
 }  // namespace util
 }  // namespace azer
