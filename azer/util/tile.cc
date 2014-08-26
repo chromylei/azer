ba17c8db4054af0da5206c4b7af5296e82f62593
lenxyang <lenxyang@gmail.com>
2014-08-26 18:04:11 +0800

    .

--- azer/util/tile.cc
+++ azer/util/tile.cc
@@ -44,21 +44,7 @@ void Tile::InitVertex() {
 }
 
 int32* Tile::InitPitchIndices(int level, const Tile::Pitch& pitch, int32* indices) {
-  const int step = std::pow(2.0f, level);
-  int32* cur = indices;
-  for (int i = pitch.top; i < pitch.bottom; i += step) {
-    for (int j = pitch.left; j < pitch.right; j += step) {
-      int cur_line = i * kGridLine;
-      int next_line = (i + step) * kGridLine;
-      *cur++ = cur_line  + j;
-      *cur++ = next_line + j;
-      *cur++ = next_line + j + step;
-      *cur++ = cur_line  + j;
-      *cur++ = next_line + j + step;
-      *cur++ = cur_line  + j + step;
-    }
-  }
-  return cur;
+  return azer::util::InitPitchIndices(level, pitch, kGridLine, indices);
 }
 
 void Tile::CalcNormal() {
@@ -186,5 +172,98 @@ void QuadTree::Split(int minlevel, Splitable* splitable,
     node++;
   }
 }
-}  // namespace azer {
+
+int32* InitPitchTopFan(const Tile::Pitch& pitch, int kGridLine,
+                       int32* indices, uint32 flags) {
+  DCHECK_EQ(pitch.right - pitch.left, pitch.bottom - pitch.top);
+  const int step = (pitch.right - pitch.left) / 2;
+
+  int topline = pitch.top * kGridLine;
+  int midline = (pitch.top + step) * kGridLine;
+  int bottomline = pitch.bottom * kGridLine;
+  int32* cur = indices;
+
+  // top
+  if (flags & 0x00000001) {
+    *cur++ = midline + step;
+    *cur++ = topline + step;
+    *cur++ = topline + pitch.left;
+
+    *cur++ = midline + step;
+    *cur++ = topline + pitch.right;
+    *cur++ = topline + step;
+    
+  } else {
+    *cur++ = midline + step;
+    *cur++ = topline + pitch.right;
+    *cur++ = topline + pitch.left;
+  }
+
+  // bottom
+  if (flags & 0x00000002) {
+    *cur++ = midline + step;
+    *cur++ = bottomline + pitch.left;
+    *cur++ = bottomline + step;
+
+    *cur++ = midline + step;
+    *cur++ = bottomline + step;
+    *cur++ = bottomline + pitch.right;
+  } else {
+    *cur++ = midline + step;
+    *cur++ = bottomline + pitch.left;
+    *cur++ = bottomline + pitch.right;
+  }
+
+  // left
+  if (flags & 0x00000004) {
+    *cur++ = midline + step;
+    *cur++ = topline + pitch.left;
+    *cur++ = midline + pitch.left;
+
+    *cur++ = midline + step;
+    *cur++ = midline + pitch.left;
+    *cur++ = bottomline + pitch.left;
+  } else {
+    *cur++ = midline + step;
+    *cur++ = topline + pitch.left;
+    *cur++ = bottomline + pitch.left;
+  }
+
+  // left
+  if (flags & 0x00000008) {
+    *cur++ = midline + step;
+    *cur++ = midline + pitch.right;
+    *cur++ = topline + pitch.right;
+
+    *cur++ = midline + step;
+    *cur++ = bottomline + pitch.right;
+    *cur++ = midline + pitch.right;
+  } else {
+    *cur++ = midline + step;
+    *cur++ = bottomline + pitch.right;
+    *cur++ = topline + pitch.right;
+  }
+
+  return cur;
+}
+
+int32* InitPitchIndices(int level, const Tile::Pitch& pitch, int kGridLine,
+                        int32* indices) {
+  const int step = std::pow(2.0f, level);
+  int32* cur = indices;
+  for (int i = pitch.top; i < pitch.bottom; i += step) {
+    for (int j = pitch.left; j < pitch.right; j += step) {
+      int cur_line = i * kGridLine;
+      int next_line = (i + step) * kGridLine;
+      *cur++ = cur_line  + j;
+      *cur++ = next_line + j;
+      *cur++ = next_line + j + step;
+      *cur++ = cur_line  + j;
+      *cur++ = next_line + j + step;
+      *cur++ = cur_line  + j + step;
+    }
+  }
+  return cur;
+}
+}  // namespace azer
 }  // namespace util
