f041513396910c5db38e05754cfc6091a5849af3
lenxyang <lenxyang@gmail.com>
2014-09-16 14:41:20 +0800

    rm rastertek

--- sandbox/rastertek/terrain/common/camera_control.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "azer/render/render.h"
-
-void CameraControl(azer::Camera* camera, azer::Radians speed, float delta_time);
-void MovableControl(azer::MovableObject* obj, azer::Radians speed, float delta_time);
