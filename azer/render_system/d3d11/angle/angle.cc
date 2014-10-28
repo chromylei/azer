0a6819fdf3270b376c89369f193af6df8373b169
lenxyang <lenxyang@gmail.com>
2014-10-28 17:05:20 +0800

    .

--- azer/render_system/d3d11/angle/angle.cc
+++ azer/render_system/d3d11/angle/angle.cc
@@ -13,30 +13,18 @@ PFNEGLQUERYSURFACEPOINTERANGLEPROC eglQuerySurfacePointerANGLE;
 namespace azer {
 namespace angle {
 EGLint configAttribList[] = {
-  EGL_RED_SIZE,       8,
-  EGL_GREEN_SIZE,     8,
-  EGL_BLUE_SIZE,      8,
+  EGL_RED_SIZE,       5,
+  EGL_GREEN_SIZE,     6,
+  EGL_BLUE_SIZE,      5,
   EGL_ALPHA_SIZE,     8,
-  EGL_DEPTH_SIZE,     EGL_DONT_CARE,
-  EGL_STENCIL_SIZE,   EGL_DONT_CARE,
+  EGL_DEPTH_SIZE,     24,
+  EGL_STENCIL_SIZE,   8,
   EGL_SAMPLE_BUFFERS, 0,
   EGL_NONE
 };
 
-EGLint surfaceAttribList[] =  {
-  EGL_WIDTH,   800,
-  EGL_HEIGHT,  600,
-  EGL_NONE, EGL_NONE
-};
-
-namespace {
-EGLDisplay eglDisplay = 0;
-EGLSurface eglSurface = 0;
-EGLContext eglContext = 0;
-}  // namespace
-
-
-bool InitAngle(window::NativeWindowHandle handle) {
+bool InitAngle(window::NativeWindowHandle handle, bool pbuffer,
+               Context* angle_context) {
   EGLint numConfigs;
   EGLint majorVersion;
   EGLint minorVersion;
@@ -68,11 +56,26 @@ bool InitAngle(window::NativeWindowHandle handle) {
     return false;
   }
 
-  surface = eglCreatePbufferSurface(display, config, surfaceAttribList);
+  if (pbuffer) {
+    EGLint surfaceAttribList[] =  {
+      EGL_WIDTH,   800,
+      EGL_HEIGHT,  600,
+      EGL_NONE, EGL_NONE
+    };
+    surface = eglCreatePbufferSurface(display, config, surfaceAttribList);
+  } else {
+    EGLint surfaceAttribList[] = {
+      EGL_POST_SUB_BUFFER_SUPPORTED_NV, EGL_TRUE,
+      EGL_NONE, EGL_NONE
+    };
+    surface = eglCreateWindowSurface(display, config,
+                                     (EGLNativeWindowType)hWnd, surfaceAttribList);
+  }
   if (surface == EGL_NO_SURFACE) {
+    LOG(ERROR) << "Failed to create Surface";
     return false;
   }
-
+  
   // Create a GL context
   context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
   if (context == EGL_NO_CONTEXT) {
@@ -84,9 +87,9 @@ bool InitAngle(window::NativeWindowHandle handle) {
     return EGL_FALSE;
   }
 
-  eglDisplay = display;
-  eglSurface = surface;
-  eglContext = context;
+  angle_context->display = display;
+  angle_context->surface = surface;
+  angle_context->context = context;
 
   eglQuerySurfacePointerANGLE =
       (PFNEGLQUERYSURFACEPOINTERANGLEPROC)eglGetProcAddress("eglQuerySurfacePointerANGLE");
@@ -97,19 +100,15 @@ bool InitAngle(window::NativeWindowHandle handle) {
   return true;
 }
 
-void UninitializeAngle() {
+void UninitializeAngle(Context* ctx) {
 }
 
-TexturePtr GetSurface() {
-  return GetSurface((int64)eglSurface);
-}
-
-TexturePtr GetSurface(int64 sur) {
+TexturePtr GetSurfaceTexture(void* sur, Context* context) {
   D3D11RenderSystem* rs = (D3D11RenderSystem*)RenderSystem::Current();
   ID3D11Texture2D* resource = NULL;
   EGLint config = EGL_D3D_TEXTURE_2D_SHARE_HANDLE_ANGLE;
   EGLSurface surface = (EGLSurface)sur;
-  if (EGL_FALSE == eglQuerySurfacePointerANGLE(eglDisplay,
+  if (EGL_FALSE == eglQuerySurfacePointerANGLE(context->display,
                                                surface,
                                                config,
                                                (void**)&resource)) {
@@ -135,6 +134,19 @@ TexturePtr GetSurface(int64 sur) {
   TexturePtr ptr(new D3D11Texture2DShared(opt, shared_tex, rs));
   // SAFE_RELEASE(resource);
   return ptr;
+}
+
+TexturePtr GetFramebufferTexture(void* framebuffer, Context* ctx) {
+  return TexturePtr();
+}
+
+TexturePtr GetCurrentFramebufferTexture(Context* ctx) {
+  EGLSurface surface = eglGetCurrentSurface(EGL_DRAW);
+  if (surface == EGL_NO_SURFACE) {
+    return TexturePtr();
+  }
+
+  return GetSurfaceTexture((void*)surface, ctx);
 }
 }  // namespace angle
 }  // namespace azer
--- azer/render_system/d3d11/angle/angle.h
+++ azer/render_system/d3d11/angle/angle.h
@@ -7,10 +7,30 @@
 
 namespace azer {
 namespace angle {
-AZER_D3D11RS_EXPORT bool InitAngle(window::NativeWindowHandle handle);
-AZER_D3D11RS_EXPORT void UninitializeAngle();
+struct Context {
+  void* display;
+  void* context;
+  void* surface;
+  void* user_data;
+  int width;
+  int height;
 
-AZER_D3D11RS_EXPORT TexturePtr GetSurface(int64 surface);
-AZER_D3D11RS_EXPORT TexturePtr GetSurface();
+  Context()
+      : display(NULL)
+      , context(NULL)
+      , surface(NULL)
+      , user_data(NULL)
+      , width(0)
+      , height(0) {
+  }
+};
+
+AZER_D3D11RS_EXPORT bool InitAngle(window::NativeWindowHandle handle, bool pbuffer,
+                                   Context* context);
+AZER_D3D11RS_EXPORT void UninitializeAngle(Context* ctx);
+
+AZER_D3D11RS_EXPORT TexturePtr GetSurfaceTexture(void* surface, Context* ctx);
+AZER_D3D11RS_EXPORT TexturePtr GetCurrentFramebufferTexture(Context* ctx);
+AZER_D3D11RS_EXPORT TexturePtr GetFramebufferTexture(void* framebuffer, Context* ctx);
 }  // namespace angle
 }  // namespace azer
